736e3de59c50621aeb12a3432c87b60304ef636f
Sam Sobell <sam.sobell@gmail.com>
2017-02-01 21:27:23 -0800

    Added a controller and basic page for the rockets command

---
+++ app/controllers/rocket_commands_controller.rb
@@ -0,0 +1,4 @@
+class RocketCommandsController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get  '/rocket_commands',   to: 'rocket_commands#index'
   resources :data_packets, :path => '/'
   root 'data_packets#index'
 end
