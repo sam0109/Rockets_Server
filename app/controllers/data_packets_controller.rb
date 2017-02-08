016c8c1da5bbbdcb2504ade8c14354031bfd053f
Sam Sobell <sam.sobell@gmail.com>
2017-02-08 14:41:24 -0800

    added D3

--- app/controllers/data_packets_controller.rb
+++ app/controllers/data_packets_controller.rb
@@ -47,6 +47,14 @@ class DataPacketsController < ApplicationController
     end
   end
 
+  def data
+    respond_to do |format|
+      format.json {
+        render :json => [1,2,3,4,5]
+      }
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_data_packet
